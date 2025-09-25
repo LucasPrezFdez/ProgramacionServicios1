4f393fb313f637f27f2fc142fe2ddc5b5f7189a6
Lukas <lucaslinoperez@gmail.com>
2025-09-25 14:48:30 +0200

    tarea sin hacer

--- src/main/java/ser1/ServerChat.java
+++ src/main/java/ser1/ServerChat.java
@@ -1,10 +1,33 @@
 package ser1;
 
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ServerChat {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
+        int puerto = 6000; // Puerto
+        // yo soy 10.101.21.152
 
+        ServerSocket servidor = new ServerSocket(puerto);
+        System.out.println("Escuchando en " + servidor.getLocalPort());
+
+        // esperando a un cliente
+        Socket cliente1 = servidor.accept();
+        // realizar acciones con cliente1
+        System.out.println("Se ha conectado el cliente.");
+        DataInputStream in = new DataInputStream(cliente1.getInputStream());
+        String msg = in.readUTF();
+        System.out.println("Mensaje recibido: "+msg );
+
+        // Preparar respuesta
+
+        System.out.println("Finalizando el programa...");
+
+        servidor.close(); // cierro socket servidor
 
     }
 }
